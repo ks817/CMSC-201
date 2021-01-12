e16b97b6caee6b08e93ba1468a3def634fa9063b
ks817 <75410496+ks817@users.noreply.github.com>
2021-01-11 19:58:20 -0500

    Add files via upload

---
+++ Labs/lab2/lab2.py
@@ -0,0 +1,15 @@
+# File:        lab2.py
+# Author:      Kusum Sharma
+# Date:        02/04/2019
+# Section:     25
+# E-mail:      ks17@umbc.edu
+# Description: This program shows the proper layout of code in python
+#              file, and greets the user with the name of the programmer.
+
+def main():
+    
+    #introduces the programmer
+    print("Hello, my name is Kusum Sharma.")
+
+
+main()
